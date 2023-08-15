fb8cd383d0d9dfc2199df539634ee0814b3e0f97
reemsarhan <115352161+reemsarhan@users.noreply.github.com>
2023-08-16 02:01:00 +0300

    Create STD_TYPES.h

---
+++ LIB/STD_TYPES.h
@@ -0,0 +1,18 @@
+
+#ifndef LIB_STD_TYPES_H_
+#define LIB_STD_TYPES_H_
+
+typedef unsigned char       u8; 		// 0 -> 256
+typedef unsigned short int u16;
+typedef unsigned long  int u32;
+
+/*signed*/
+typedef signed char         s8;
+typedef signed short int   s16;
+typedef signed long  int   s32;
+
+/*float*/
+typedef float              f32;
+typedef double             f64;
+
+#endif /* LIB_STD_TYPES_H_ */
